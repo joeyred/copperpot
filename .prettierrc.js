49c7c9ab9e0f18ed88a86aeb4ed18dc3ca1c38eb
Brian Hayes <bjoeyhayes@gmail.com>
2022-01-09 22:27:58 -0500

    init

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports ={
+  tabWidth: 2,
+  printWidth: 60,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# CopperPot
+
+So Many Untilites!
+
+An ADHD centeralization of all the untility type things I've written but never made properly reuseable.
